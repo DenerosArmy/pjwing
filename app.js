c41d0fc915e7027fb7d13d6ec5084fc0ee848d92
Vaishaal <vaishaal@berkeley.edu>
2012-06-23 16:12:53 -0700

    deleted everything

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 
+
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , swig = require('swig');
 
 var app = module.exports = express.createServer();
 
 // Configuration
+app.register(".html",swig);
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
+  app.set('view options', {layout: false});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-
+swig.init({ 
+        root: __dirname + '/views',
+        allowErrors: true
+});
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -28,8 +35,7 @@ app.configure('production', function(){
 });
 
 // Routes
-
-app.get('/', routes.index);
+app.get('/',routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
